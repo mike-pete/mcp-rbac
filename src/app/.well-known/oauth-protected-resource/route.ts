51661807476684ddf52926fa2536ebfde643e37f
Mike Peterson <mike@lemonshell.com>
2025-08-31 20:36:51 -0700

    wip

---
+++ src/app/.well-known/oauth-protected-resource/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import { env } from "../../../env";
+
+export async function GET(request: NextRequest) {
+  const url = new URL(request.url);
+  const baseUrl = `${url.protocol}//${url.host}`;
+  
+  return NextResponse.json({
+    resource: `${baseUrl}/api/mcp`,
+    authorization_servers: [env.WORKOS_AUTHKIT_DOMAIN],
+    bearer_methods_supported: ["header"],
+    scopes_supported: ["openid", "profile", "email"], // todo: might not need these
+  }, {
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "public, max-age=3600",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
+
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
